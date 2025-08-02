379418795e0faca070f096660a91d1b671f6e565
Himanshu gaur <himanshugaur055@gmail.com>
2025-08-02 19:33:29 +0530

    otp

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,8 +1,9 @@
 // src/firebase.js
 import { initializeApp } from "firebase/app";
-import { getAuth, RecaptchaVerifier, signInWithPhoneNumber } from "firebase/auth";
+import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { getStorage } from 'firebase/storage';
+import { getStorage } from "firebase/storage";
+
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
@@ -12,8 +13,10 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
+
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
-export const storage = getStorage(app);
-export { auth, RecaptchaVerifier, signInWithPhoneNumber, db };
+const storage = getStorage(app);
+
+export { auth, db, storage };
