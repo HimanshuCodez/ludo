7e458cab271b6a810e1d363a3dde2ebf6b649f75
Himanshu gaur <himanshugaur055@gmail.com>
2025-08-03 12:03:33 +0530

    pay upi changee

--- frontend/src/pages/Pay.jsx
+++ frontend/src/pages/Pay.jsx
@@ -51,7 +51,7 @@ export function Pay() {
       return;
     }
 
-    const upiLink = `upi://pay?pa=khushisantoshi21@okaxis&pn=TrueWinCircle&am=${amount}&cu=INR&tr=${transactionId}`;
+    const upiLink = `upi://pay?pa=7982461603@yapl&pn=TrueWinCircle&am=${amount}&cu=INR&tr=${transactionId}`;
     window.location.href = upiLink; // Open UPI app
     window.localStorage.setItem('TransactionId', transactionId); // Store transaction ID
     setLoading(false);
